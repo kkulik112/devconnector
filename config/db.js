b6f863217ab1a83818138fd5c48c2af3f790ee4c
kkulik112 <kkulik112@gmail.com>
2022-12-18 13:41:29 +0100

    Setting up test routes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try{
-        await mongoose.set('strictQuery', false)
+        mongoose.set('strictQuery', false)
         await mongoose.connect(db)
         console.log('database connected...')
     }catch(err){
