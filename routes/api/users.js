dbcd5d047f2e5d6f29fbbc61bfd87cdf80357789
kkulik112 <kkulik112@gmail.com>
2022-12-20 19:50:26 +0100

    Returning jwt on user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {check, validationResult} = require('express-validator')
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 
@@ -44,9 +46,17 @@ router.post('/', [
         await user.save()
 
         // TODO return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User registered')
-
+        jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000},(err, token) => {
+            if(err) throw err
+            res.json({token})
+        })
+        
     }catch(err){
         console.error(err.message)
         res.status(500).send('Server error')
