7adccd6d92b3a849ad487759fe199bc27bf32332
kkulik112 <kkulik112@gmail.com>
2022-12-28 20:25:12 +0100

    Returning profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -91,4 +91,33 @@ router.post('/', [auth, [
     }
 })
 
+// @route GET /api/profile
+// @desc Returns all user profiles
+// @access Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json({profiles})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route GET /api/profile/user/:user_id
+// @desc Returns profile by user ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar'])
+        if(!profile) return res.status(400).json({msg: 'Profile not found'})
+        res.json({profile})
+    } catch (err) {
+        console.error(err.message)
+        // Check error type to handle invalid user_id
+        if(err.kind == 'ObjectId') return res.status(400).json({msg: 'Profile not found'})
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router
