53be3106036f91f4bcd304899d48846aef47cc1b
kkulik112 <kkulik112@gmail.com>
2023-01-04 20:59:34 +0100

    Added register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+    
+    const {name, email, password, confirmPassword} = formData
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+    const onSubmit = e => {
+        e.preventDefault()
+        if(password !== confirmPassword){
+            console.log('Passwords do not match')
+        } else {
+            console.log(formData)
+        }
+    }
+    
+    return(
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={onChange} required/>
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} onChange={onChange}
+            minLength="6"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={confirmPassword} onChange={onChange}
+            minLength="6"
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </Fragment>
+    )
 }
 
 export default Register
