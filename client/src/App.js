c35d74003497e7132ba3353a4441c8010de81d62
kkulik112 <kkulik112@gmail.com>
2023-01-04 20:25:01 +0100

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import {Fragment} from 'react'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 
 const App = () => 
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Routes>
+        <Route exact path='/' element={<Landing/>} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/register' element={<Register/>} />
+        </Routes>
+      </section>
     </Fragment>
+  </Router>
 
 export default App;
