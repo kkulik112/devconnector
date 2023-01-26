ac9ff512ffc95e1b0b7d5d0d17af6b262a7df5f1
kkulik112 <kkulik112@gmail.com>
2023-01-26 18:48:35 +0100

    Fetching profiles and github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import {setAlert} from '../actions/alert'
-import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED } from './types'
+import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED , GET_PROFILES, GET_REPOS} from './types'
 
 // Get current user's profile
 export const getCurrentProfile = () => async dispatch => {
@@ -18,6 +18,59 @@ export const getCurrentProfile = () => async dispatch => {
     }
 }
 
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({type: GET_PROFILES})
+    try {
+        const res = await axios.get('/api/profile')
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Get github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`)
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Create profile
 export const createProfile = (formData, navigate, edit=false) => async dispatch => {
     try {
         const config = {
