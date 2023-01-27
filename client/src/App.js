ece6db9bcd74f44849da7c084cab28ef16c778af
kkulik112 <kkulik112@gmail.com>
2023-01-27 20:50:50 +0100

    Displaying profiles

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
+import Profiles from './components/profiles/Profiles'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
 import PrivateRoute from './components/routing/PrivateRoute'
@@ -42,6 +43,7 @@ const App = () => {
           <Routes>
             <Route exact path='login' element={<Login/>}/>
             <Route exact path='register' element={<Register/>} />
+            <Route exact path='profiles' element={<Profiles/>} />
             <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -20,7 +20,7 @@ export const getCurrentProfile = () => async dispatch => {
 
 // Get all profiles
 export const getProfiles = () => async dispatch => {
-    dispatch({type: GET_PROFILES})
+    dispatch({type: CLEAR_PROFILE})
     try {
         const res = await axios.get('/api/profile')
 
