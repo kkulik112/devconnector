6c909777f2cdc014e44e9fd12dbcda03587941ab
kkulik112 <kkulik112@gmail.com>
2023-01-29 14:14:38 +0100

    Displaying basic profile info

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import {getProfileById} from '../../actions/profile'
 
 const Profile = ({getProfileById, profile: {profile, loading}, auth}) => {
@@ -20,6 +22,10 @@ const Profile = ({getProfileById, profile: {profile, loading}, auth}) => {
             <Fragment>
                 <Link to="/profiles" className="btn btn-light">Back To Profiles</Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to="/edit-profile" className='btn btn-dark'>Edit Profile</Link>}
+                <div className="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
+                </div>
             </Fragment>)}
         </Fragment>
     )
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -61,7 +61,7 @@ router.post('/', [auth, [
     if(company) profileFields.company = company
     if(website) profileFields.website = website
     if(location) profileFields.location = location
-    if(bio) profileFields.bio = bio
+    profileFields.bio = bio ? bio : ''
     if(status) profileFields.status = status
     if(githubusername) profileFields.githubusername = githubusername
     if(skills) {
