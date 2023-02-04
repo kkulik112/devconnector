666333dca90f841d59308e0837010d480730436f
kkulik112 <kkulik112@gmail.com>
2023-02-04 22:34:17 +0100

    Setting production environment

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
 // Connect database
 connectDB()
-
-app.get('/', (req, res) => res.send('API running'))
 
 // Init middleware
 app.use(express.json({extended: false}))
@@ -16,6 +15,14 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
